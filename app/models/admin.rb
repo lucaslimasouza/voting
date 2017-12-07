93744f130cf41c3535878afa93137925711e02af
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2017-12-07 21:18:33 -0200

    Test - Admin should have many meetings

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,6 @@
 class Admin < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :meetings,  dependent: :destroy
 end
