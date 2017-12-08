d040eb8287f83c17db9bad9a640137ee729c4e5a
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2017-12-07 22:14:16 -0200

    Test - Vote should validate that :candidate_id is unique within the scope of :user_id

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,5 @@ class Vote < ApplicationRecord
   belongs_to :candidate
 
   validates :user_id, :candidate_id, presence: true
+  validates_uniqueness_of :candidate_id, scope: :user_id
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe Vote, type: :model do
     %i( user_id candidate_id ).each do |field|
       it { is_expected.to validate_presence_of field }
     end
+
+    it {
+      create(:vote)
+      is_expected.to validate_uniqueness_of(:candidate_id).scoped_to(:user_id)
+    }
   end
 end
