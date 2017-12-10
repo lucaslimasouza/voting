41921e65a3a40135544f8721d4098f33ff2262ce
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2017-12-10 15:19:27 -0200

    Test fixed - Vote should validate taht :candidate_id is case-sensitively unique within the scope of :job_role_id and :user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207234445) do
+ActiveRecord::Schema.define(version: 20171210170841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,7 +78,9 @@ ActiveRecord::Schema.define(version: 20171207234445) do
     t.bigint "candidate_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "job_role_id"
     t.index ["candidate_id"], name: "index_votes_on_candidate_id"
+    t.index ["job_role_id"], name: "index_votes_on_job_role_id"
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
@@ -86,5 +88,6 @@ ActiveRecord::Schema.define(version: 20171207234445) do
   add_foreign_key "job_roles", "meetings"
   add_foreign_key "meetings", "admins"
   add_foreign_key "votes", "candidates"
+  add_foreign_key "votes", "job_roles"
   add_foreign_key "votes", "users"
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
   describe 'validation' do
-    %i( user_id candidate_id ).each do |field|
+    %i( user_id candidate_id job_role_id ).each do |field|
       it { is_expected.to validate_presence_of field }
     end
 
     it {
       create(:vote)
-      is_expected.to validate_uniqueness_of(:candidate_id).scoped_to(:user_id)
+      is_expected.to validate_uniqueness_of(:candidate_id)
+        .scoped_to([:job_role_id, :user_id])
     }
   end
 end
